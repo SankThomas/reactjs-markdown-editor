59ad78c56307dd3e735d4886e1273e179c56707a
Thomas Sankara <thomassankara74@gmail.com>
2021-09-10 11:27:08 +0300

    main App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import { Remarkable } from "remarkable"
 
-function App() {
+const md = new Remarkable()
+
+export default function App() {
+  const [text, setText] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main className="px-5 py-10 text-gray-900 md:max-w-3xl md:mx-auto">
+      <h1 className="font-bold text-center text-4xl xl:text-6xl mb-5">
+        Markdown editor
+      </h1>
 
-export default App;
+      <article className="flex flex-col items-start justify-center rounded">
+        <label htmlFor="markdown" className="mb-3">
+          Type in something to see the magic
+        </label>
+        <textarea
+          name="textarea"
+          id="markdown"
+          cols="30"
+          rows="10"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="w-full text-gray-900 tracking-wide p-5 rounded shadow"
+          placeholder="Type in something"
+        ></textarea>
+      </article>
+
+      <h3 className="mt-5 font-bold tracking-wide text-xl">Output</h3>
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: md.render(text) }}
+      ></div>
+
+      <footer className="fixed bottom-5">
+        <h3>Built in ReactJs using Remarkable</h3>
+      </footer>
+    </main>
+  )
+}
